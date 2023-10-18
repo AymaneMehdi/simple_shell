038992a8708841812db4190903b8bbe2995cc28a
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-10-18 15:13:24 -0700

    Update builtin_emulators2.c

--- builtin_emulators2.c
+++ builtin_emulators2.c
@@ -6,10 +6,9 @@
  * @info: Structure containing potential arguments.
  * Return: Always 0.
  */
-int my_history(info_t *info)
-{
-print_list(info->history);
-return 0;
+int my_history(info_t *info) {
+    print_list(info->history);
+    return 0;
 }
 
 /**
@@ -18,20 +17,17 @@ return 0;
  * @str: The string alias to remove.
  * Return: 0 on success, 1 on error.
  */
-int unset_alias(info_t *info, const char *str)
-{
-char *p, c;
-int ret;
+int unset_alias(info_t *info, const char *str) {
+    char *equal_sign = _strchr((char *)str, '=');
 
-p = _strchr((char *)str, '=');
-if (!p)
-return 1;
-c = *p;
-*p = 0;
-ret = delete_node_at_index(&(info->alias),
-get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-*p = c;
-return ret;
+    if (equal_sign) {
+        *equal_sign = '\0'; // Null terminate the string at the position of '='
+        int result = delete_node_at_index(&(info->alias),
+                                          get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+        *equal_sign = '='; // Restore the original string
+        return result;
+    }
+    return 1;
 }
 
 /**
@@ -40,18 +36,16 @@ return ret;
  * @str: The string alias to add/update.
  * Return: 0 on success, 1 on error.
  */
-int set_alias(info_t *info, const char *str)
-{
-char *p;
+int set_alias(info_t *info, const char *str) {
+    char *equal_sign = _strchr((char *)str, '=');
 
-p = _strchr((char *)str, '=');
-if (!p)
-return 1;
-if (!*++p)
-return unset_alias(info, str);
-
-unset_alias(info, str);
-return (add_node_end(&(info->alias), str, 0) == NULL);
+    if (equal_sign) {
+        *equal_sign = '\0'; // Null terminate the string at the position of '='
+        unset_alias(info, str);
+        *equal_sign = '=';
+        return (add_node_end(&(info->alias), str, 0) == NULL);
+    }
+    return 1;
 }
 
 /**
@@ -59,21 +53,19 @@ return (add_node_end(&(info->alias), str, 0) == NULL);
  * @node: The alias node.
  * Return: 0 on success, 1 on error.
  */
-int print_alias(list_t *node)
-{
-char *p = NULL, *a = NULL;
-
-if (node)
-{
-p = _strchr(node->str, '=');
-for (a = node->str; a <= p; a++)
-_putchar(*a);
-_putchar('\'');
-_puts(p + 1);
-_puts("'\n");
-return 0;
-}
-return 1;
+int print_alias(list_t *node) {
+    if (node) {
+        char *equal_sign = _strchr(node->str, '=');
+        if (equal_sign) {
+            for (char *a = node->str; a <= equal_sign; a++)
+                _putchar(*a);
+            _putchar('\'');
+            _puts(equal_sign + 1);
+            _puts("'\n");
+            return 0;
+        }
+    }
+    return 1;
 }
 
 /**
@@ -81,30 +73,21 @@ return 1;
  * @info: Structure containing potential arguments.
  * Return: Always 0.
  */
-int my_alias(info_t *info)
-{
-int i = 0;
-char *p = NULL;
-list_t *node = NULL;
-
-if (info->argc == 1)
-{
-node = info->alias;
-while (node)
-{
-print_alias(node);
-node = node->next;
-}
-return 0;
-}
-for (i = 1; info->argv[i]; i++)
-{
-p = _strchr(info->argv[i], '=');
-if (p)
-set_alias(info, info->argv[i]);
-else
-print_alias(node_starts_with(info->alias, info->argv[i], '='));
-}
-
-return 0;
+int my_alias(info_t *info) {
+    if (info->argc == 1) {
+        list_t *node = info->alias;
+        while (node) {
+            print_alias(node);
+            node = node->next;
+        }
+    } else {
+        for (int i = 1; info->argv[i]; i++) {
+            char *equal_sign = _strchr(info->argv[i], '=');
+            if (equal_sign)
+                set_alias(info, info->argv[i]);
+            else
+                print_alias(node_starts_with(info->alias, info->argv[i], '='));
+        }
+    }
+    return 0;
 }
