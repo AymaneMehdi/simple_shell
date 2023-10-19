cd91838418347a33b4298fd6d041b1a08f445ce3
Henhrie <akinolahenryayobami@gmail.com>
2023-10-19 16:38:56 +0800

    simple shell

--- env.c
+++ env.c
@@ -3,19 +3,19 @@
 /**
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *  constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
 {
-    char **env_ptr = environ;
-    while (*env_ptr != NULL)
-    {
-        _puts(*env_ptr);
-        _putchar('\n');
-        env_ptr++;
-    }
-    return (0);
+char **env_ptr = environ;
+while (*env_ptr != NULL)
+{
+_puts(*env_ptr);
+_putchar('\n');
+env_ptr++;
+}
+return (0);
 }
 
 /**
@@ -27,17 +27,17 @@ int _myenv(info_t *info)
  */
 char *_getenv(info_t *info, const char *name)
 {
-    char **env_ptr = environ;
-    while (*env_ptr != NULL)
-    {
-        if (_strncmp(*env_ptr, name, _strlen(name)) == 0)
-        {
-            char *value = *env_ptr + _strlen(name) + 1;
-            return value;
-        }
-        env_ptr++;
-    }
-    return NULL;
+char **env_ptr = environ;
+while (*env_ptr != NULL)
+{
+if (_strncmp(*env_ptr, name, _strlen(name)) == 0)
+{
+char *value = *env_ptr + _strlen(name) + 1;
+return value;
+}
+env_ptr++;
+}
+return NULL;
 }
 
 /**
@@ -49,23 +49,23 @@ char *_getenv(info_t *info, const char *name)
  */
 int _mysetenv(info_t *info)
 {
-    if (info->argc != 3)
-    {
-        _eputs("Incorrect number of arguments\n");
-        return (1);
-    }
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguments\n");
+return (1);
+}
 
-    char *name = info->argv[1];
-    char *value = info->argv[2];
+char *name = info->argv[1];
+char *value = info->argv[2];
 
-    int success = setenv(name, value, 1);
-    if (success == -1)
-    {
-        perror("Error setting environment variable");
-        return (1);
-    }
+int success = setenv(name, value, 1);
+if (success == -1)
+{
+perror("Error setting environment variable");
+return (1);
+}
 
-    return (0);
+return (0);
 }
 
 /**
@@ -76,17 +76,17 @@ int _mysetenv(info_t *info)
  */
 int _myunsetenv(info_t *info)
 {
-    int i;
-    for (i = 1; i < info->argc; i++)
-    {
-        int success = unsetenv(info->argv[i]);
-        if (success == -1)
-        {
-            perror("Error unsetting environment variable");
-            return (1);
-        }
-    }
-    return (0);
+int i;
+for (i = 1; i < info->argc; i++)
+{
+int success = unsetenv(info->argv[i]);
+if (success == -1)
+{
+perror("Error unsetting environment variable");
+return (1);
+}
+}
+return (0);
 }
 
 /**
@@ -97,9 +97,9 @@ int _myunsetenv(info_t *info)
  */
 int populate_env_list(info_t *info)
 {
-    // No need to populate, environ is already populated.
-    // The environment variables are stored in the global variable `environ`.
-    // `environ` is an array of strings where each string has the format "NAME=VALUE".
-    info->env = NULL; // Set to NULL since we are not using a linked list.
-    return (0);
+    /**No need to populate, environ is already populated.
+    /**The environment variables are stored in the global variable `environ`.
+    /**`environ` is an array of strings where each string has the format "NAME=VALUE".*/
+info->env = NULL; /**Set to NULL since we are not using a linked list.*/
+return (0);
 }
