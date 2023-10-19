a4bdfe9d8c07929280fcb9e26e82c83e6f548466
Henhrie <akinolahenryayobami@gmail.com>
2023-10-19 16:32:48 +0800

    simple shell

--- builtin_emulators.c
+++ builtin_emulators.c
@@ -1,6 +1,28 @@
 #include "shell.h"
 #include <stdio.h>
 
+/**
+ * my_exit - Set error-related values in the 'info' structure based on
+ * command-line arguments.
+ * This function is responsible for managing program exit statuses and
+ * error conditions.
+ * It takes a pointer to an 'info_t' structure as a parameter, which is
+ * expected to contain relevant program information. The function performs
+ * the following actions:
+ * - If a command-line argument exists at index 1, it attempts to convert it
+ * into an integer and sets 'exit_code' accordingly.
+ * - If 'exit_code' is -1, the 'info' structure's 'status' is set to 2,
+ * indicating an error, and an error message is printed. The function
+ * returns 1 to signify an error exit.
+ * - If 'exit_code' is a valid integer, it sets the 'err_num' field in
+ * the 'info' structure.
+ * - If no command-line argument is found at index 1, 'err_num' is set to -1,
+ * indicating the absence of a specific error number.
+ *
+ * Return Value:
+ * The function returns -2, and the interpretation of this return value
+ * depends on the larger context within which the function is used.
+ */
 int my_exit(info_t *info)
 {
 if (info->argv[1])
@@ -21,6 +43,21 @@ info->err_num = -1;
 return (-2);
 }
 
+/**
+ * my_cd - Change the current working directory.
+ *
+ * This function changes the current working directory based on the provided
+ * directory path.
+ * If no directory path is given, it attempts to change to the user's
+ * home directory or the present working directory as appropriate.
+ * If the provided directory path is "-", it changes to the previous working
+ * directory.
+ * @info: Parameter structure.
+ *
+ * Return:
+ * - 0 on success.
+ * - 1 on error (with an error message printed in the case of chdir failure).
+ */
 int my_cd(info_t *info)
 {
 char *dir = info->argv[1];
@@ -57,6 +94,17 @@ _setenv(info, "PWD", getcwd(buffer, sizeof(buffer)));
 return (0);
 }
 
+/**
+ * my_help - Display a help message (placeholder).
+ *
+ * This function is a placeholder for displaying a help message.
+ * It currently prints a message indicating that the help functionality
+ * is not yet implemented.
+ * @info: Parameter structure.
+ *
+ * Return:
+ * Always returns 0 to signify that the function has been called.
+ */
 int my_help(info_t *info)
 {
 _puts("Help call works. Function not yet implemented.\n");
