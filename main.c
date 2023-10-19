f62577640b7c37ad515ec92e76b7ef73f7285707
Henhrie <akinolahenryayobami@gmail.com>
2023-10-19 20:30:46 +0800

    simple shell

--- main.c
+++ main.c
@@ -1,29 +1,34 @@
 #include "shell.h"
 
-int main(int argc, char **argv) {
-    info_t info = INFO_INIT;
-    int fd = 2;
+int main(int argc, char **argv)
+{
+info_t info = INFO_INIT;
+int fd = 2;
 
-    fd += 3;
+fd += 3;
 
-    if (argc == 2) {
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1) {
-            if (errno == EACCES) {
-                exit(126);
-            }
-            if (errno == ENOENT) {
-                fprintf(stderr, "%s: 0: Can't open %s\n", argv[0], argv[1]);
-                exit(127);
-            }
-            return EXIT_FAILURE;
-        }
-        info.readfd = fd;
-    }
+if (argc == 2)
+{
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
+{
+if (errno == EACCES)
+{
+exit(126);
+}
+if (errno == ENOENT)
+{
+fprintf(stderr, "%s: 0: Can't open %s\n", argv[0], argv[1]);
+exit(127);
+}
+return EXIT_FAILURE;
+}
+info.readfd = fd;
+}
 
-    populate_env_list(&info);
-    read_history(&info);
-    hsh(&info, argv);
+populate_env_list(&info);
+read_history(&info);
+hsh(&info, argv);
 
-    return EXIT_SUCCESS;
+return EXIT_SUCCESS;
 }
