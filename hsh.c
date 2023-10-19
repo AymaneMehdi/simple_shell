d3fa9b326bcf6053a8550ddc15a7f5cf75375f45
Henhrie <akinolahenryayobami@gmail.com>
2023-10-19 20:22:06 +0800

    simple shell

--- hsh.c
+++ hsh.c
@@ -26,13 +26,15 @@ int hsh(info_t *info, char **av)
 ssize_t r = 0;
 int builtin_ret = 0;
 
-while (r != -1 && builtin_ret != -2) {
+while (r != -1 && builtin_ret != -2)
+{
 clear_info(info);
 if (interactive(info))
 _puts("$ ");
 _eputchar(BUF_FLUSH);
 r = get_input(info);
-if (r != -1) {
+if (r != -1)
+{
 set_info(info, av);
 builtin_ret = find_builtin(info);
 if (builtin_ret == -1)
@@ -46,7 +48,8 @@ write_history(info);
 free_info(info, 1);
 if (!interactive(info) && info->status)
 exit(info->status);
-if (builtin_ret == -2) {
+if (builtin_ret == -2)
+{
 if (info->err_num == -1)
 exit(info->status);
 exit(info->err_num);
@@ -54,7 +57,8 @@ exit(info->err_num);
 return (builtin_ret);
 }
 
-int find_builtin(info_t *info) {
+int find_builtin(info_t *info)
+{
 int i, built_in_ret = -1;
 builtin_table builtintbl[] = {
 {"exit", _myexit},
@@ -69,15 +73,17 @@ builtin_table builtintbl[] = {
 };
 
 for (i = 0; builtintbl[i].type; i++)
-if (_strcmp(info->argv[0], builtintbl[i].type) == 0) {
+if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+{
 info->line_count++;
 built_in_ret = builtintbl[i].func(info);
 break;
 }
-return built_in_ret;
+return (built_in_ret);
 }
 
-void find_cmd(info_t *info) {
+void find_cmd(info_t *info)
+{
 char *path = NULL;
 
     /**(rest of your find_cmd implementation) */
@@ -89,7 +95,7 @@ fork_cmd(info);
 }
 else
 {
-        /**(rest of your find_cmd implementation) */
+/**(rest of your find_cmd implementation) */
 }
 }
 
@@ -98,7 +104,8 @@ void fork_cmd(info_t *info)
 pid_t child_pid;
 
 child_pid = fork();
-if (child_pid == -1) {
+if (child_pid == -1)
+{
 perror("Error:");
 return;
 }
@@ -115,7 +122,8 @@ exit(1);
 else
 {
 wait(&(info->status));
-if (WIFEXITED(info->status)) {
+if (WIFEXITED(info->status))
+{
 info->status = WEXITSTATUS(info->status);
 if (info->status == 126)
 print_error(info, "Permission denied\n");
