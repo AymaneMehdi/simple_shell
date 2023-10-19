54501943692c4cc060af361c74460e3d6f6ce51b
Henhrie <akinolahenryayobami@gmail.com>
2023-10-19 20:52:12 +0800

    simple shell

---
+++ string_a.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _strcpy - Copies a string
+ * @dest: The destination buffer
+ * @src: The source string
+ *
+ * Return: Pointer to destination
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int i = 0;
+
+if (dest == src || src == NULL)
+return dest;
+
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+
+dest[i] = '\0';
+return dest;
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @str: The string to duplicate
+ *
+ * Return: Pointer to the duplicated string or NULL on failure
+ */
+char *_strdup(const char *str)
+{
+if (str == NULL)
+return NULL;
+
+int length = 0;
+while (str[length])
+length++;
+
+char *ret = malloc(sizeof(char) * (length + 1));
+if (ret == NULL)
+return NULL;
+
+for (int i = 0; i <= length; i++)
+ret[i] = str[i];
+
+return ret;
+}
+
+/**
+ * _puts - Prints a string to stdout
+ * @str: The string to be printed
+ */
+void _puts(const char *str)
+{
+if (str == NULL)
+return;
+
+for (int i = 0; str[i]; i++)
+_putchar(str[i]);
+}
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, 1. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+static int i;
+static char buf[WRITE_BUF_SIZE];
+
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(1, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+
+return 1;
+}
---
+++ tokenizer.c
@@ -0,0 +1,122 @@
+#include "shell.h"
+
+/**
+ * **strtow - Splits a string into words using multiple delimiters.
+ * @str: The input string.
+ * @delimiters: The delimiter string.
+ *
+ * Return: A pointer to an array of strings, or NULL on failure.
+ */
+char **strtow(char *str, char *delimiters)
+{
+int i, j, k, m, numwords = 0;
+char **words;
+
+if (str == NULL || str[0] == '\0')
+return (NULL);
+
+if (!delimiters)
+delimiters = " ";
+
+// Count the number of words
+for (i = 0; str[i] != '\0'; i++)
+{
+if (!is_delimiter(str[i], delimiters) && (is_delimiter(str[i + 1], delimiters) || str[i + 1] == '\0'))
+numwords++;
+}
+
+if (numwords == 0)
+return (NULL);
+
+words = malloc((numwords + 1) * sizeof(char *));
+if (!words)
+return (NULL);
+
+for (i = 0, j = 0; j < numwords; j++)
+{
+while (is_delimiter(str[i], delimiters))
+i++;
+
+k = 0;
+while (!is_delimiter(str[i + k], delimiters) && str[i + k] != '\0')
+k++;
+
+words[j] = malloc((k + 1) * sizeof(char));
+if (!words[j])
+{
+for (k = 0; k < j; k++)
+free(words[k]);
+free(words);
+return (NULL);
+}
+
+for (m = 0; m < k; m++)
+words[j][m] = str[i++];
+
+ words[j][m] = '\0';
+}
+
+words[j] = NULL;
+return (words);
+}
+
+/**
+ * **strtow2 - Splits a string into words using a single delimiter.
+ * @str: The input string.
+ * @delimiter: The delimiter character.
+ *
+ * Return: A pointer to an array of strings, or NULL on failure.
+ */
+char **strtow2(char *str, char delimiter)
+{
+int i, j, k, m, numwords = 0;
+char **words;
+
+if (str == NULL || str[0] == '\0')
+return (NULL);
+
+// Count the number of words
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((str[i] != delimiter && str[i + 1] == delimiter) ||
+(str[i] != delimiter && str[i + 1] == '\0') ||
+str[i + 1] == delimiter)
+{
+numwords++;
+}
+}
+
+if (numwords == 0)
+return (NULL);
+
+words = malloc((numwords + 1) * sizeof(char *));
+if (!words)
+return (NULL);
+
+for (i = 0, j = 0; j < numwords; j++)
+{
+while (str[i] == delimiter && str[i] != '\0')
+i++;
+
+k = 0;
+while (str[i + k] != delimiter && str[i + k] != '\0')
+k++;
+
+words[j] = malloc((k + 1) * sizeof(char));
+if (!words[j])
+{
+for (k = 0; k < j; k++)
+free(words[k]);
+free(words);
+return (NULL);
+}
+
+for (m = 0; m < k; m++)
+words[j][m] = str[i++];
+
+words[j][m] = '\0';
+}
+
+words[j] = NULL;
+return (words);
+}
